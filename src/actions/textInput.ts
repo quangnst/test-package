cdd104e25daa7d509b37fd2d3bd503592a5d5881
NITECO\quang.nghiem <quang.nghiem@niteco.se>
2025-02-26 09:45:02 +0700

    test update api

--- src/actions/textInput.ts
+++ src/actions/textInput.ts
@@ -1,6 +1,5 @@
-"use server";
-
 export async function updateTextInput(apiUrl: string, payload: any) {
+  "use server";
   console.log("payload", payload, apiUrl);
   const res = await fetch(apiUrl, {
     method: "POST",
